96596d55cbcfa1ce7de57c9d87480039bf8a715e
Stewart Bentley <51534918+sbentley2019@users.noreply.github.com>
2019-11-05 18:35:22 -0500

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,14 +1,15 @@
-const sentence = "hello there from lighthouse labs";
-let time = 0;
-let count = 0;
-
-for (const char of sentence) {
-  setTimeout(() => {
-    process.stdout.write(char);
-    count--;
-    if (!count) {
-      process.stdout.write('\n');
-    }
-  }, time += 60);
-  count++;
+const delayLetters = function(str) {
+  let time = 0;
+  for (let i of str) {
+    setTimeout(() => {
+      process.stdout.write(i);
+      if (i === str[str.length - 1]) {
+        process.stdout.write('\n');
+      }
+    }, time * 50);
+    time++;
+  }
 }
+
+const sentence = "hello there from lighthouse labs";
+delayLetters(sentence);
